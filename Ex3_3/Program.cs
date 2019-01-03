26a7b49a9afc4d389a0562e4f01f3f27b6c16f5d
AmirElamineLDS <Amir.Elamine@se.linedata.com>
2019-01-03 15:33:30 +0100

    Ended Ex3 from Training 3

---
+++ Ex3_3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex3_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write Something");
+            string StringVar = Console.ReadLine();
+            string[] revStringVar=StringVar.Split(' ');
+            string revVarCh;
+            foreach(string varCh in revStringVar)
+            {
+                revVarCh = "";
+                for (int i = varCh.Length - 1; i >= 0; i--)
+                {
+                    revVarCh = revVarCh + varCh[i];
+                    
+                }
+                
+                Console.Write(revVarCh+" ");
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
